befbfc8d1b722154ae295de8ef4b32f08390d3e0
DKichukov <dzhamal.kichukov@gmail.com>
2025-07-31 09:20:17 +0300

    Implement retrieve photo endpoint

--- src/main/java/com/project/restaurant_review/config/SecurityConfig.java
+++ src/main/java/com/project/restaurant_review/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.project.restaurant_review.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -17,6 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/photos/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
--- src/main/java/com/project/restaurant_review/controller/PhotoController.java
+++ src/main/java/com/project/restaurant_review/controller/PhotoController.java
@@ -3,11 +3,19 @@ package com.project.restaurant_review.controller;
 import com.project.restaurant_review.dto.PhotoDto;
 import com.project.restaurant_review.service.PhotoService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import static org.springframework.http.MediaTypeFactory.getMediaType;
 
 @RestController
 @AllArgsConstructor
@@ -20,5 +28,19 @@ public class PhotoController {
     public PhotoDto uploadPhoto(@RequestParam("file") MultipartFile file) {
         return photoService.uploadPhoto(file);
     }
+
+    @GetMapping(path = "/{fileName:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String fileName) {
+        return photoService.getPhotoAsResource(fileName).map(photo ->
+                        ResponseEntity.ok()
+                                .contentType(getMediaType(photo)
+                                        .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                                .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                                .body(photo))
+                .orElse(ResponseEntity.notFound()
+                        .build());
+
+    }
+
 
 }
